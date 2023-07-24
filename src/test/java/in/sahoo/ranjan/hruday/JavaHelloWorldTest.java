11a36ced73fc834681d4ccd6a7d9f1634fca3ad8
dangerouspython <hrudayranjansahoo0208@gmail.com>
2023-07-24 23:22:26 +0530

    Initialized release-plugin demo from SpringframeworkGuru

---
+++ src/test/java/in/sahoo/ranjan/hruday/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package in.sahoo.ranjan.hruday;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println(new JavaHelloWorld().getHello());
+    }
+
+}
